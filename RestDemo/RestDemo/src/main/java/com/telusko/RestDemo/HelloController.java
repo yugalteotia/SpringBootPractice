f928091b5f044e1bc86dc0984bee151a0974c6b0
Yugal Teotia <67860100+yugalteotia@users.noreply.github.com>
2023-12-12 02:05:43 +0530

    Add files via upload

---
+++ RestDemo/RestDemo/src/main/java/com/telusko/RestDemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.telusko.RestDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World!";
+    }
+}
